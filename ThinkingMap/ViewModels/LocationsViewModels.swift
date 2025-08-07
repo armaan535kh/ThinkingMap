addb85ecfc764d0e4037c9c1624460fb9b74ae6f
armaan535kh <kh.armaan535@gmail.com>
2025-08-07 10:13:35 +0530

    [Update] Created ViewModel wiht @StateObject and @EnvironmentObject for MVVM

---
+++ ThinkingMap/ViewModels/LocationsViewModels.swift
@@ -0,0 +1,8 @@
+//
+//  LocationsViewModels.swift
+//  ThinkingMap
+//
+//  Created by Armaan Khan  on 07/08/25.
+//
+
+import Foundation
---
+++ ThinkingMap/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  ThinkingMap
+//
+//  Created by Armaan Khan  on 07/08/25.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}

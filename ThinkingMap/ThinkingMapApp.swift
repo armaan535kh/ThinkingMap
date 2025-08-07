14184a9192975d8644fa27dba19c480eff9f26e9
armaan535kh <kh.armaan535@gmail.com>
2025-08-07 08:38:51 +0530

    Initial Commit

---
+++ ThinkingMap/ThinkingMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkingMapApp.swift
+//  ThinkingMap
+//
+//  Created by Armaan Khan  on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ThinkingMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThinkingMapTests/ThinkingMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkingMapTests.swift
+//  ThinkingMapTests
+//
+//  Created by Armaan Khan  on 07/08/25.
+//
+
+import Testing
+@testable import ThinkingMap
+
+struct ThinkingMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

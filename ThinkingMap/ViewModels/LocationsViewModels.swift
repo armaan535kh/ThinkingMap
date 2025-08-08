2cee0002a1080fba2f6b556640322d9f49cea657
armaan535kh <kh.armaan535@gmail.com>
2025-08-08 11:59:30 +0530

    [Update] Created the location view model

--- ThinkingMap/ViewModels/LocationsViewModels.swift
+++ ThinkingMap/ViewModels/LocationsViewModels.swift
@@ -6,12 +6,29 @@
 //
 
 import Foundation
+import MapKit
 
 class LocationViewModel: ObservableObject {
     @Published var locations: [Location]
     
+    @Published var mapLocation: Location
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinate, span: mapSpan
+            )
+        
     }
 }
+

4dc60933053457a247f7847dcfa3594f92a9e292
armaan535kh <kh.armaan535@gmail.com>
2025-08-10 08:06:46 +0530

    [Feature] Created a header to show cityname and place

--- ThinkingMap/Views/LocationsView.swift
+++ ThinkingMap/Views/LocationsView.swift
@@ -20,6 +20,9 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion )
                 .ignoresSafeArea()
+            header
+                .padding()
+            
         }
     }
 }
@@ -27,4 +30,31 @@ struct LocationsView: View {
 #Preview {
     LocationsView()
         .environmentObject(LocationViewModel())
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack{
+            VStack(spacing: 0) {
+                Text(vm.mapLocation.name+", "+vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundStyle(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                
+                
+            }.background(.thickMaterial)
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+            
+            
+            Spacer()
+        }
+            
+            
+        
+        
+        
+    }
 }

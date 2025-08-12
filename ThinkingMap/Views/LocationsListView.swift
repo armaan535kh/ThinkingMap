f130ee92a3b36368ee45cef69fa7ce3711cf120d
armaan535kh <kh.armaan535@gmail.com>
2025-08-12 08:45:38 +0530

    [Feature] Created locationListView for header

---
+++ ThinkingMap/Views/LocationsListView.swift
@@ -0,0 +1,48 @@
+//
+//  LocationsListView.swift
+//  ThinkingMap
+//
+//  Created by Armaan Khan  on 12/08/25.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    @EnvironmentObject private var vm: LocationViewModel
+    var body: some View {
+        List{
+            ForEach(vm.locations){ location in 
+                ListRowView(location: location)
+                
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    LocationsListView()
+        .environmentObject(LocationViewModel())
+}
+
+
+extension LocationsListView {
+    private func ListRowView(location: Location) -> some View {
+        HStack {
+            if let imageName = location.imageName.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 45, height: 45)
+            }
+            
+            VStack(alignment: .leading){
+                Text(location.name)
+                    .font(.headline)
+                Text(location.cityName)
+                    .font(.subheadline)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
